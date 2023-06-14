05159ee8c9b091cfa135bc07f35582babe3b6020
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-14 10:25:19 +0200

    default firebase configs added to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slack-clone-group';
+
+  /* import { Firestore, collection, onSnapshot, addDoc, setDoc, doc } from '@angular/fire/firestore'; // Imports
+
+  constructor(private firestore: Firestore) {} // Constructor import
+
+  onSnapshot(collection(this.firestore, 'users'), (snapshot) => { //retrieve data
+        let user = snapshot.docs
+          .map((doc) => {
+            let data = doc.data();
+            let id = doc.id;
+            return { id, ...data };
+          })
+          .find((user) => user.id === this.userID); // if you want find custom user
+
+        if (user) {
+          this.user = new User(user);
+        }
+      });
+
+
+
+   try {
+        let usersCollectionRef = collection(this.firestore, 'users');  // set data in database
+        let userDocRef = doc(usersCollectionRef, this.userID);
+
+        await setDoc(userDocRef, this.user.toJSON());
+
+        this.loadingProgress = false;
+      } catch (e) {
+        console.error('Error updating document: ', e);
+      }
+   */
+
 }
