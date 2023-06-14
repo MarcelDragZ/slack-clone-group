b2e75b43d2eca441ca81a40f637a45d339b5c8f5
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-14 10:11:30 +0200

    firebase deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-group',
+    appId: '1:123460881012:web:41fbbfe2a5651dbbe09f21',
+    storageBucket: 'slack-clone-group.appspot.com',
+    apiKey: 'AIzaSyCdxbmxeh-XUa0OzuJkapTbXJOt4UWrZ5Q',
+    authDomain: 'slack-clone-group.firebaseapp.com',
+    messagingSenderId: '123460881012',
+    measurementId: 'G-Q8B8BYSM0Z',
+  },
+};
