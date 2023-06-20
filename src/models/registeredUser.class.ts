001a9cb2ad8de328c4277e1f823f380c80f7f88a
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-20 12:31:12 +0200

    private channels added to registeredUsers

--- src/models/registeredUser.class.ts
+++ src/models/registeredUser.class.ts
@@ -1,16 +1,19 @@
 export class RegisteredUser {
   email: string;
   password: string;
+  privateChannels: object;
 
   constructor(obj?: any) {
     this.email = obj ? obj.email : '';
     this.password = obj ? obj.password : '';
+    this.privateChannels = obj ? obj.privateChannels : '';
   }
 
   public toJSON() {
     return {
       email: this.email,
       password: this.password,
+      privateChannels: this.privateChannels,
     };
   }
 }
