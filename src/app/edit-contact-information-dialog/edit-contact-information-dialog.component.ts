cdf9bae2c1f524ca362fc9892164e1313d15bd6c
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-23 00:43:53 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/MarcelDragZ/slack-clone-group""
    This reverts commit f1d1dcebc13853c8531ea9a7d4bf391e6648b155.

--- src/app/edit-contact-information-dialog/edit-contact-information-dialog.component.ts
+++ src/app/edit-contact-information-dialog/edit-contact-information-dialog.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface DialogData {
+  currentUser: any;
+}
 
 @Component({
   selector: 'app-edit-contact-information-dialog',
   templateUrl: './edit-contact-information-dialog.component.html',
-  styleUrls: ['./edit-contact-information-dialog.component.scss']
+  styleUrls: ['./edit-contact-information-dialog.component.scss'],
 })
 export class EditContactInformationDialogComponent {
+  constructor(
+    public firestore: AngularFirestore,
+    public dialogRef: MatDialogRef<EditContactInformationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
+  saveChanges() {
+    this.firestore
+      .collection('registeredUsers')
+      .doc(this.data.currentUser.id) // Use the id of the currentUser
+      .update(this.data.currentUser) // We do not need to call .toJSON() here
+      .then(() => {
+        this.dialogRef.close();
+      })
+      .catch((error) => {
+        console.error('Error updating document: ', error);
+      });
+  }
 }
