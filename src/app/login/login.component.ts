eaf8262c456bd113dd845a575ceea6123218ffad
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-22 12:10:24 +0200

    completed sing in with google and sign in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, collection, onSnapshot, addDoc, setDoc, doc } from '@angular/fire/firestore';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
+import * as bcrypt from 'bcryptjs';
+import { provideAuth, getAuth, GoogleAuthProvider } from '@angular/fire/auth';
+import { signInWithPopup } from '@firebase/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-user;
+  user;
+  email;
+  password;
 
-constructor(private firestore: Firestore) {}
+  constructor(private firestore: Firestore, private router: Router) {}
 
-ngOnInit(): void {
-  onSnapshot(collection(this.firestore, 'registeredUsers'), (snapshot) => { //retrieve data
-    let user = snapshot.docs
-      .map((doc) => {
+  ngOnInit(): void {
+    onSnapshot(collection(this.firestore, 'registeredUsers'), (snapshot) => {
+      let user = snapshot.docs.map((doc) => {
         let data = doc.data();
         let id = doc.id;
         return { id, ...data };
-      })
+      });
       this.user = user;
-  });
-}
+    });
+  }
 
-logInUser() {
-  console.log(this.user);
+  logInUser() {
+    let user = this.user.find((user) => user.email === this.email);
 
- }
+    if (user) {
+      bcrypt.compare(this.password, user.password, (err, result) => {
+        if (result) {
+          this.router.navigate(['/workspace']);
+          console.log('Erfolgreich eingeloggt');
+        } else {
+          console.log('Falsches Passwort');
+        }
+      });
+    } else {
+      console.log('Benutzer nicht gefunden');
+    }
+  }
 
+  async loginWithGoogle() {
+    let auth = getAuth();
+    let provider = new GoogleAuthProvider();
+
+    try {
+      let result = await signInWithPopup(auth, provider);
+      let user = result.user;
+      this.router.navigate(['/workspace']);
+      console.log('Erfolgreich eingeloggt mit Google:', user);
+    } catch (error) {
+      console.error('Fehler bei der Google-Anmeldung:', error);
+    }
+  }
 }
