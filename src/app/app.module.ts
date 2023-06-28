74ed36de59d98cb29ce8969febce674ddda699b7
Marcel Herzog <119870192+MarcelDragZ@users.noreply.github.com>
2023-06-28 12:34:32 +0200

    added storage for pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp, getApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, GoogleAuthProvider } from '@angular/fire/auth';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
@@ -62,6 +63,7 @@ import { QuillModule } from 'ngx-quill';
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
     BrowserAnimationsModule,
     MatSidenavModule,
     MatIconModule,
--- src/app/message-panel/message-panel.component.ts
+++ src/app/message-panel/message-panel.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { getDownloadURL, getStorage, ref } from '@angular/fire/storage';
+import { Observable, from } from 'rxjs';
 @Component({
   selector: 'app-message-panel',
   templateUrl: './message-panel.component.html',
   styleUrls: ['./message-panel.component.scss']
 })
-export class MessagePanelComponent {
+export class MessagePanelComponent implements OnInit {
+  imageUrl: Observable<string>;
 
+  constructor() {}
+
+  ngOnInit() {
+    const filePath = 'gs://slack-clone-group.appspot.com/Avatarbackground.png'; // Der Pfad zu deinem Bild in Firebase Storage
+    const storageRef = ref(getStorage(), filePath);
+    this.imageUrl = from(getDownloadURL(storageRef));
+    console.log(this.imageUrl);
+
+  }
 }
